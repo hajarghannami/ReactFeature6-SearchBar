79ae75b1a4df5078f751e0e489ab923584b384e2
hajarghannami <hajarghannami@outlook.com>
2021-01-24 16:13:25 +0300

    hey

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,12 @@ import { SearchBarStyled } from "../styles";
 const SearchBar = (props) => {
   return (
     <SearchBarStyled
-      onChange={(event) => console.log(event.target.value)}
+      onChange={(event) => props.setQuery(event.target.value)}
       placeholder="Search for a cookie name"
     />
   );
 };
+
+console.log("setQuery");
 
 export default SearchBar;
